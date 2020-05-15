b9ac0c1b5a39720749651c3d85e8afcf0d5fef50
Yaroslav <encored@i.ua>
2020-05-15 17:41:10 +0300

    add build

---
+++ client/build/precache-manifest.76c570a0dd246a354a9352a29cbde92a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79d43e1cff06a8f6f4378f0626d0307",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6341e419b750a0157c25",
+    "url": "/static/css/main.46a17e50.chunk.css"
+  },
+  {
+    "revision": "2c2c30b03a869a4020c4",
+    "url": "/static/js/2.dcb580d3.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/static/js/2.dcb580d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6341e419b750a0157c25",
+    "url": "/static/js/main.c565fb56.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "0ccf9bc553e278efa4969d7a8e2abe52",
+    "url": "/static/media/arena.0ccf9bc5.jpg"
+  },
+  {
+    "revision": "0a6375c660c126978e7ec49532524d1b",
+    "url": "/static/media/versus.0a6375c6.png"
+  }
+]);
